d690bcc40ec0a1a5003148d84d688ad986504e59
jxxnixx <dkepffk10@gmail.com>
2024-08-04 01:23:24 +0900

    feat: API + CORS Setting

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+	reactStrictMode: true,
 	transpilePackages: ['three'],
 	sassOptions: {
 		additionalData: `@use '/src/scss/abstracts/index' as *;`,
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/:path*',
+				destination: process.env.NEXT_PUBLIC_LOOGLE_BASE + '/:path*',
+			},
+		]
 	},
 }
 
