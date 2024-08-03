c63df0791508776a52581a9043479821f22cbe2d
jxxnixx <dkepffk10@gmail.com>
2024-08-03 16:04:30 +0900

    fix: Glass Text Animation

--- src/component/glassText.tsx
+++ src/component/glassText.tsx
@@ -1,91 +1,74 @@
 'use client'
 
-import { Suspense, useEffect, useState } from 'react'
-import { ContactShadows, Environment, Float, Lightformer, MeshTransmissionMaterial, Text3D } from '@react-three/drei'
+import { Suspense, useEffect, useRef, useState } from 'react'
+import {
+	ContactShadows,
+	Environment,
+	Float,
+	Image,
+	Lightformer,
+	MeshTransmissionMaterial,
+	Text3D,
+	useTexture,
+} from '@react-three/drei'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import { easing } from 'maath'
+import * as THREE from 'three'
 
 type Props = {
-	children?: string
+	character: string
+	index: number
 }
 
-const GlassText = ({ children }: Props) => {
+const GlassText = ({ character, index }: Props) => {
+	const [hovered, setHovered] = useState(false)
+	const [position, setPosition] = useState(new THREE.Vector3(index * 2 - 6, 0.5, 0))
+	const targetPosition = new THREE.Vector3(index * 2 - 6, hovered ? 1 : 0.5, 0)
+
+	useFrame((state, delta) => {
+		position.lerp(targetPosition, delta * 5)
+		setPosition(position.clone())
+	})
+
 	return (
-		<Text3D
-			font='/font/Lonely Coffee_Regular.json'
-			size={2}
-			height={1}
-			curveSegments={12}
-			bevelEnabled
-			bevelThickness={0.3}
-			bevelSize={0.04}
-			bevelOffset={0}
-			bevelSegments={5}
-			position={[-5, 0.5, 0]}>
-			{children}
-			<MeshTransmissionMaterial
-				thickness={0.5}
-				roughness={0.1}
-				transmission={1}
-				ior={1.5}
-				specularIntensity={1}
-				color='#c1d8ff'
-			/>
-			{/* <meshPhysicalMaterial
-				color='#c1d8ff'
-				transmission={1}
-				opacity={1}
-				roughness={0}
-				metalness={0}
-				clearcoat={1}
-				iridescence={1}
-				ior={2}
-				clearcoatRoughness={1}
-				reflectivity={1}
-				transparent
-				specularIntensity={1}
-			/> */}
-		</Text3D>
+		<Float floatIntensity={2.5} speed={index + 1}>
+			<Text3D
+				font='/font/Lonely Coffee_Regular.json'
+				size={2}
+				height={1}
+				curveSegments={12}
+				bevelEnabled
+				bevelThickness={0.3}
+				bevelSize={0.04}
+				bevelOffset={0}
+				bevelSegments={5}
+				position={position}
+				onPointerOver={() => setHovered(true)}
+				onPointerOut={() => setHovered(false)}>
+				{character}
+				<MeshTransmissionMaterial
+					opacity={1}
+					metalness={0}
+					roughness={0}
+					transmission={1}
+					ior={1.5}
+					thickness={0}
+					specularIntensity={1}
+					specularColor='white'
+					envMapIntensity={1}
+					color={'0xffffff'}
+				/>
+			</Text3D>
+		</Float>
 	)
 }
 
-const Rig = () => {
-	const [cursorVisible, setCursorVisible] = useState(true)
-	const { gl } = useThree()
-
-	useEffect(() => {
-		const handleMouseLeave = () => setCursorVisible(false)
-		const handleMouseEnter = () => setCursorVisible(true)
-
-		gl.domElement.addEventListener('mouseleave', handleMouseLeave)
-		gl.domElement.addEventListener('mouseenter', handleMouseEnter)
-
-		return () => {
-			gl.domElement.removeEventListener('mouseleave', handleMouseLeave)
-			gl.domElement.removeEventListener('mouseenter', handleMouseEnter)
-		}
-	}, [gl])
-
-	useFrame((state, delta) => {
-		const pointer = cursorVisible ? state.pointer : { x: 0, y: 0 }
-		easing.damp3(
-			state.camera.position,
-			[Math.sin(-pointer.x) * 5, Math.sin(-pointer.y) * 5, 15 + Math.cos(pointer.x) * 3],
-			0.2,
-			delta
-		)
-		state.camera.lookAt(0, 0, 0)
-	})
-
-	return null
-}
-
 const Scene = () => {
+	const text = 'LOOGLE'
+
 	return (
 		<div style={{ width: '100vw', height: '100vh' }}>
-			<Canvas camera={{ position: [0, 0, 20], fov: 50 }}>
-				<color attach='background' args={['#c1d8ff']} />
-
+			<Canvas camera={{ position: [0, 0, 20], fov: 50 }} style={{ backgroundImage: '/image/file.png' }}>
+				<color attach='background' args={['#fff']} />
 				<spotLight position={[20, 20, -10]} penumbra={1} castShadow angle={0.2} />
 				<ambientLight intensity={0.5} />
 				<directionalLight intensity={0.5} position={[-5, 5, -15]} />
@@ -93,23 +76,20 @@ const Scene = () => {
 
 				<ContactShadows scale={100} position={[0, -5.5, 0]} blur={1} far={100} opacity={0.85} />
 
-				<Environment>
-					<color attach='background' args={['#c1d8ff']} />
+				<Environment files='/image/5.hdr' backgroundIntensity={1}>
 					<Lightformer
-						intensity={8}
+						intensity={20}
 						position={[10, 5, -10]}
-						scale={[10, 50, 1]}
+						scale={[1, 50, 1]}
 						onUpdate={(self) => self.lookAt(0, 0, 0)}
 					/>
 				</Environment>
 
 				<Suspense fallback={null}>
-					<Float floatIntensity={2}>
-						<GlassText>LOOGLE</GlassText>
-					</Float>
+					{text.split('').map((char, index) => (
+						<GlassText key={index} character={char} index={index} />
+					))}
 				</Suspense>
-
-				<Rig />
 
 				<ambientLight intensity={Math.PI / 2} />
 				<directionalLight intensity={1} castShadow position={[1, 1, 10]} />
