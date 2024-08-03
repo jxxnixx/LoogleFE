9fdfa042b1bcef7b3438a98c8092166a3866a70d
jxxnixx <dkepffk10@gmail.com>
2024-08-03 19:28:04 +0900

    fix: GlassText Background

--- src/component/glassText.tsx
+++ src/component/glassText.tsx
@@ -11,8 +11,9 @@ import {
 	Text3D,
 	useTexture,
 } from '@react-three/drei'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
 import * as THREE from 'three'
+import { TextureLoader } from 'three'
 
 type Props = {
 	character: string
@@ -45,20 +46,37 @@ const GlassText = ({ character, index }: Props) => {
 				onPointerOver={() => setHovered(true)}
 				onPointerOut={() => setHovered(false)}>
 				{character}
+
 				<MeshTransmissionMaterial
+					color={'lightblue'}
 					opacity={1}
 					metalness={0}
 					roughness={0}
 					transmission={1}
-					ior={1.5}
+					ior={3}
 					thickness={0}
 					specularIntensity={1}
 					specularColor='white'
 					envMapIntensity={1}
-					color={'0xffffff'}
+					reflectivity={1}
+					iridescence={0}
+					iridescenceIOR={1}
+					clearcoat={1}
+					clearcoatRoughness={0}
 				/>
 			</Text3D>
 		</Float>
+	)
+}
+const BackgroundPlane = () => {
+	const backgroundTexture = useLoader(TextureLoader, '/image/gradientBackground.png')
+
+	return (
+		<mesh castShadow receiveShadow>
+			<color attach='background' args={['white']} />
+			<meshBasicMaterial color={'#fff'} />
+			{/* <meshBasicMaterial map={backgroundTexture} opacity={1} color={'#fff'} /> */}
+		</mesh>
 	)
 }
 
@@ -66,33 +84,43 @@ const Scene = () => {
 	const text = 'LOOGLE'
 
 	return (
-		<div style={{ width: '100vw', height: '100vh' }}>
-			<Canvas camera={{ position: [0, 0, 20], fov: 50 }} style={{ backgroundImage: '/image/file.png' }}>
-				<color attach='background' args={['#fff']} />
-				<spotLight position={[20, 20, -10]} penumbra={1} castShadow angle={0.2} />
+		<div style={{ width: '100vw', height: '100vh', backgroundColor: '#fff' }}>
+			<Canvas camera={{ position: [0, 0, 20], fov: 50 }}>
+				{/* <color attach='background' args={['#fff']} /> */}
+
+				{/* <spotLight position={[20, 20, -10]} penumbra={1} castShadow angle={0.2} />
 				<ambientLight intensity={0.5} />
 				<directionalLight intensity={0.5} position={[-5, 5, -15]} />
-				<pointLight intensity={0.5} position={[-5, -5, -10]} />
+				<pointLight intensity={0.5} position={[-5, -5, -10]} /> */}
+				{/* 
+				<pointLight intensity={1} position={[10, 10, 10]} />
+				<pointLight intensity={1} position={[-10, -10, 10]} />
+				<pointLight intensity={1} position={[10, -10, -10]} />
+				<pointLight intensity={1} position={[-10, 10, -10]} />
+				<pointLight intensity={1} position={[0, 10, 0]} />
+				<pointLight intensity={1} position={[0, -10, 0]} /> */}
 
-				<ContactShadows scale={100} position={[0, -5.5, 0]} blur={1} far={100} opacity={0.85} />
+				{/* <ContactShadows scale={100} position={[0, -5.5, 0]} blur={1} far={100} opacity={0.85} /> */}
 
-				<Environment files='/image/5.hdr' backgroundIntensity={1}>
-					<Lightformer
+				{/* <Environment preset='dawn' background> */}
+				<Environment files='/image/sky.hdr' background backgroundBlurriness={0.2} backgroundIntensity={2}>
+					{/* <Lightformer
 						intensity={20}
-						position={[10, 5, -10]}
+						position={[10, 50, -10]}
 						scale={[1, 50, 1]}
 						onUpdate={(self) => self.lookAt(0, 0, 0)}
-					/>
+					/> */}
 				</Environment>
 
 				<Suspense fallback={null}>
+					<BackgroundPlane />
 					{text.split('').map((char, index) => (
 						<GlassText key={index} character={char} index={index} />
 					))}
 				</Suspense>
 
-				<ambientLight intensity={Math.PI / 2} />
-				<directionalLight intensity={1} castShadow position={[1, 1, 10]} />
+				{/* <ambientLight intensity={Math.PI / 2} /> */}
+				{/* <directionalLight intensity={1} castShadow position={[1, 1, 10]} /> */}
 			</Canvas>
 		</div>
 	)
