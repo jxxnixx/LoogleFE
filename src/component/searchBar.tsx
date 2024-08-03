a38b94423d22269dcbd665324be884b762bb4d5f
jxxnixx <dkepffk10@gmail.com>
2024-08-03 16:04:15 +0900

    feat: Basic Search Bar

---
+++ src/component/searchBar.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import React, { useState } from 'react'
+
+import Icon from './base/Icon'
+
+import styles from './searchBar.module.scss'
+
+const SearchBar = () => {
+	const [searchValue, setSearchValue] = useState('')
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchValue(event.target.value)
+	}
+
+	return (
+		<label className={styles.searchBar}>
+			<Icon path='magnifier' alt='search' className={styles.magnifier} />
+			<input
+				type='text'
+				placeholder='Search'
+				className={styles.input}
+				value={searchValue}
+				onChange={handleInputChange}
+			/>
+			<Icon path='camera' alt='camera' className={styles.camera} />
+		</label>
+	)
+}
+
+export default SearchBar
