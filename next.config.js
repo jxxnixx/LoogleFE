30d86de36bf23376bfda40d756eb8d4e1e4bf3c9
jxxnixx <dkepffk10@gmail.com>
2024-08-04 21:23:45 +0900

    feat: Redirect

--- next.config.js
+++ next.config.js
@@ -7,12 +7,28 @@ const nextConfig = {
 	sassOptions: {
 		additionalData: `@use '/src/scss/abstracts/index' as *;`,
 	},
-
 	async rewrites() {
 		return [
 			{
 				source: '/:path*',
 				destination: process.env.NEXT_PUBLIC_LOOGLE_BASE + '/:path*',
+			},
+		]
+	},
+	async redirects() {
+		return [
+			{
+				source: '/:path*',
+				has: [
+					{
+						type: 'custom',
+						value: (req) => {
+							return !req.query?.path
+						},
+					},
+				],
+				destination: '/',
+				permanent: false,
 			},
 		]
 	},
