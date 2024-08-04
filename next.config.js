8026ed538f8ba609dad791bbff7afc79da4331d3
born2slayy <122500877+born2slayy@users.noreply.github.com>
2024-08-04 17:42:44 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,14 +9,6 @@ const nextConfig = {
 		additionalData: `@use '/src/scss/abstracts/index' as *;`,
 	},
 
-	async rewrites() {
-		return [
-			{
-				source: '/:path*',
-				destination: `${process.env.NEXT_PUBLIC_LOOGLE_BASE}/:path*`,
-			},
-		];
-	},
 
 	images: {
 		unoptimized: true,
