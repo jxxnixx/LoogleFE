dac8059bcd5086358a261f83fbe0e496bb129b48
born2slayy <122500877+born2slayy@users.noreply.github.com>
2024-08-04 18:33:41 +0900

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-# Stop the existing process
-pm2 stop all
 
-# Start the new process
-pm2 start /home/ubuntu/front/server.js --name looglefront
+REPOSITORY=/home/ubuntu/front
+REPOSITORY_PROD=/home/ubuntu/front
+DEPLOYMENT_GROUP_NAME= looglefront-codedeploy-deployment-group
+
+echo "DEPLOYMENT_GROUP_NAME: ${DEPLOYMENT_GROUP_NAME}"
+
+echo "운영 서버 배포"
+cd "${REPOSITORY_PROD}"
+sudo npm install
+pm2 describe til-product > /dev/null
+if [ $? -eq 0 ]; then
+  # 실행 중인 경우
+  echo "til-product 프로세스가 실행 중입니다."
+  sudo npm run pm2:reload:prod
+else
+  # 실행 중이 아닌 경우
+  echo "til-product 프로세스가 실행되지 않았습니다."
+  sudo npm run pm2:start:prod
+fi
+
