9ebc23a9f48aca2c0d307dab57015f10a7bafa88
born2slayy <122500877+born2slayy@users.noreply.github.com>
2024-08-04 17:08:45 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -32,3 +32,15 @@ const withImages = require('next-images')
 module.exports = withImages()
 
 module.exports = nextConfig
+
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',  // 백엔드 API 주소
+      },
+    ];
+  },
+};
