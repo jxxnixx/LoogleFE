bad90be37995a3a848f8f1ac12b72ce4d6378d79
YeonJu Kim <dkepffk10@sookmyung.ac.kr>
2024-08-05 16:57:35 +0900

    fix: Github Action Deploy

--- src/app/api/deploy/route.ts
+++ src/app/api/deploy/route.ts
@@ -3,11 +3,12 @@ import { NextResponse } from 'next/server'
 export async function POST(req: Request) {
 	const response = await fetch(
 		'https://api.github.com/repos/jxxnixx/LoogleFE/actions/workflows/deploy.yml/dispatches',
+		// 'https://api.github.com/repos/jxxnixx/LoogleFE/actions/workflows/deploy.yml/dispatches',
 		{
 			method: 'POST',
 			headers: {
 				Authorization: `token ${process.env.DEPLOY_TOKEN}`,
-				Accept: 'application/vnd.github.v3+json',
+				Accept: 'application/vnd.github+json',
 			},
 			body: JSON.stringify({
 				ref: 'main',
