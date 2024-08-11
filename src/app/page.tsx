51e929f1fec0c08820147fefadc3866a2c77077d
jxxnixx <dkepffk10@gmail.com>
2024-08-11 23:23:56 +0900

    fix: CSS Details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,6 @@ import styles from './landing.module.scss'
 export default function Home() {
 	return (
 		<main className={styles.landing}>
-			{/* <FillImg src='/image/gradientBackground.png' alt='background' className={styles.background} /> */}
-			{/* <FillImg src='/image/file.png' alt='background' className={styles.background} /> */}
 			<div className={styles.searchBarArea}>
 				<SearchBar width='50%' height='60%' minWidth='380px' landing />
 			</div>
